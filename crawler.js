c8b5487d29fe2ae42a6d4c57a1fda333bd6f2d2b
mathieu <mathieupheron1@gmail.com>
2021-04-21 20:45:57 +0200

    First commit : crawler.js + package.json :cactus:

---
+++ crawler.js
@@ -0,0 +1,3 @@
+const request = require('request'); // make HTTP requests
+const cheerio = require('cheerio'); // parse and select HTML element on the page
+const URL = require('url-parse'); // used to parse URLs
