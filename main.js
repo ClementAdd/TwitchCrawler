abe57a4b7e87ff1475dbe5ce69f2f95aff637284
Clément Addario <clement.addario@ynov.com>
2021-04-30 17:29:57 +0200

    Change main.js, Start to DuckDuckGo and finish to the Twitch page

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await page.screenshot({ path: 'example.png' });
 
-    await browser.close();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto('https://duckduckgo.com/');
+
+    await page.type('#search_form_input_homepage', 'twitch')
+    await page.keyboard.press('ArrowDown', { delay: 200 });
+    await page.keyboard.press('Enter',{ delay: 2000 });
+    //await page.click("#Twitch")
+    for (let i = 0; i < 12; i++) {
+        await page.keyboard.press('Tab');
+    }
+    await page.keyboard.press('Enter');  // ACCES A TWITCH DEPUIS DUCKDUCKGO
+    //await browser.close();
 })();
+
+
