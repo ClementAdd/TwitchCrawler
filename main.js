e37081f728e1109435889b8948c1e70f128f2b2c
mathieu <mathieupheron1@gmail.com>
2021-05-03 16:26:13 +0200

    add page.click for going to twitch

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-
-    await page.setViewport({ width: 1920, height: 1080 });
+    await page.setViewport({width: 1920, height: 1080});
     await page.goto('https://duckduckgo.com/');
 
     await page.type('#search_form_input_homepage', 'twitch')
-    await page.keyboard.press('ArrowDown', { delay: 200 });
-    await page.keyboard.press('Enter',{ delay: 2000 });
+    await page.keyboard.press('ArrowDown', {delay: 200});
+    await page.keyboard.press('Enter', {delay: 2000});
     //await page.click("#Twitch")
-    for (let i = 0; i < 12; i++) {
-        await page.keyboard.press('Tab');
-    }
-    await page.keyboard.press('Enter',{ delay: 5000 });  // ACCES A TWITCH DEPUIS DUCKDUCKGO
-    for (let i = 0; i < 7; i++) {
-        await page.keyboard.press('Tab',{ delay: 10 });
-    }
-    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'Parcourir'
 
-    for (let i = 0; i < 24; i++) {
-        await page.keyboard.press('Tab',{ delay: 10 });
-    }
-    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'chaine Live'
-    for (let i = 0; i < 3; i++) {
-        await page.keyboard.press('Tab',{ delay: 10 });
-    }
-    await page.keyboard.press('Enter',{ delay: 5000 })
-    for (let i = 0; i < 2; i++) {
-        await page.keyboard.press('Tab',{ delay: 10 });
-    }
-    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'chaine Live triés par viewers croissants'
-    //await browser.close();
+    const twitch = await page.$x('//*[@id="sl-0"]/a'); // Access to twitch/directory from duckduckgo
+    await twitch[0].click();
+
+    // await new Promise(r => setTimeout(r, 2000)); // delay for load page
+    // const directory = await page.$x('//*[@id="root"]/div/div[2]/nav/div/div[1]/div[1]/div/div[1]/div[1]/a'); // Accès à 'Parcourir'
+    // await directory[0].click(); // Accès à 'Parcourir'
+    //
+
+    // await new Promise(r => setTimeout(r, 5000));// delay for load page
+    // const liveChannel = await page.$x('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div/div/div[3]/div[1]/div/ul/li[2]/a');
+    // await liveChannel[0].click(); // Acess to Chaine Live
+    //
+    // await new Promise(r => setTimeout(r, 2000));// delay for load page
+    // const sortBy = await page.$x('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div/div/div[3]/div[2]/div[2]/div[2]/div/div[1]/button');
+    // await sortBy[0].click();// Acces to the sort list
+    // await new Promise(r => setTimeout(r, 2000));// delay for load page
+    //
+    // const sortDecrease = await page.$x('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div/div/div[3]/div[2]/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div[2]/a');
+    // await sortDecrease[0].click();// Accès à 'chaine Live triés par viewers croissants'
+    // await new Promise(r => setTimeout(r, 2000));// delay for load page
+    //
+    // await browser.close();
 })();
 
 
