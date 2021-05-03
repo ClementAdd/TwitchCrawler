885c11b5276862cd672b37271e5aa74a182305d1
Clément Addario <clement.addario@ynov.com>
2021-05-03 11:06:43 +0200

    Go to channels twitch sorted by viewer

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.setViewport({ width: 1366, height: 768 });
+    await page.setViewport({ width: 1920, height: 1080 });
     await page.goto('https://duckduckgo.com/');
 
     await page.type('#search_form_input_homepage', 'twitch')
@@ -16,9 +16,22 @@ const puppeteer = require('puppeteer');
     }
     await page.keyboard.press('Enter',{ delay: 5000 });  // ACCES A TWITCH DEPUIS DUCKDUCKGO
     for (let i = 0; i < 7; i++) {
-        await page.keyboard.press('Tab');
+        await page.keyboard.press('Tab',{ delay: 10 });
     }
-    await page.keyboard.press('Enter'); // Accès à 'Parcourir'
+    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'Parcourir'
+
+    for (let i = 0; i < 24; i++) {
+        await page.keyboard.press('Tab',{ delay: 10 });
+    }
+    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'chaine Live'
+    for (let i = 0; i < 3; i++) {
+        await page.keyboard.press('Tab',{ delay: 10 });
+    }
+    await page.keyboard.press('Enter',{ delay: 5000 })
+    for (let i = 0; i < 2; i++) {
+        await page.keyboard.press('Tab',{ delay: 10 });
+    }
+    await page.keyboard.press('Enter',{ delay: 5000 }); // Accès à 'chaine Live triés par viewers croissants'
     //await browser.close();
 })();
 
