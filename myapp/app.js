9dd3a6d79772194f787205244439e43f409810ba
mathieu <mathieupheron1@gmail.com>
2021-05-05 17:04:05 +0200

    can open html file with express

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 80;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
