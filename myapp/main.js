0705199a8335afab5ce711a6a566ab228a8df940
Clément Addario <clement.addario@ynov.com>
2021-05-07 15:13:50 +0200

    Fix channel name

--- myapp/main.js
+++ myapp/main.js
@@ -1,29 +1,33 @@
 const puppeteer = require('puppeteer');
 
-async function getStats(){
+async function getStats() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     await page.goto('https://www.twitch.tv/directory/all?sort=VIEWER_COUNT');
     await new Promise(r => setTimeout(r, 3000));
 
-    const streamers = await page.$$eval("[data-target='directory-game__card_container']", (streams)=>{
+    const streamers = await page.$$eval("[data-target='directory-game__card_container']", (streams) => {
         let streamers = [];
-        streams.forEach((stream)=>{
+        streams.forEach((stream) => {
 
             const nameAndCate = stream.querySelector(".tw-media-card-meta__links").innerText.split(/\n+/);
             let name = nameAndCate[0]
-            if(name.indexOf('(') !== -1){
+            if (name.indexOf('(') !== -1) {
                 name = name.split('(')
                 name = name[1].split(')')
                 name = name[0].replace(',', '')
             }
 
+
+            name.replace(' ', '')
+
+
             streamers.push({
-                link: 'https://twitch.tv/' + name,
+                link: 'https://twitch.tv/' + name.replaceAll(' ', ''),
                 title: stream.querySelector('.tw-media-card-meta__title').innerText,
                 nameStreamer: nameAndCate[0],
-                category:   nameAndCate[1],
+                category: nameAndCate[1],
                 viewers: stream.querySelector('.tw-media-card-stat').innerText,
                 miniature: stream.querySelector('.tw-c-text-overlay .tw-image').src,
                 pdp: stream.querySelector('.tw-avatar .tw-image-avatar').src,
@@ -37,8 +41,6 @@ async function getStats(){
 
     return streamers
 }
-
-
 
 
 module.exports = {getStats}
--- utils/utils.js
+++
@@ -1,4 +0,0 @@
-export const sleep = (ms) => {
-    sleeping = await new Promise(r => setTimeout(r, ms));
-    return sleeping;
-}
