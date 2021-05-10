1e8802039a3a8d2b235747e0772033b1e8a0ccd6
Clément Addario <clement.addario@ynov.com>
2021-05-10 15:02:45 +0200

    add a reload button

--- myapp/app.js
+++ myapp/app.js
@@ -5,9 +5,10 @@
     const port = 8080;
 
     const main = require("./main")
-    const streamers = await main.getStats();
+    let streamers = await main.getStats();
 
-    app.get('/', function (req, res) {
+    app.get('/', async function (req, res) {
+        streamers = await main.getStats();
         res.sendFile(path.join(__dirname, './public/index.html'));
     });
     app.get('/about-us', function (req, res) {
@@ -17,10 +18,6 @@
     app.get('/streamers', function (req, res) {
         res.send(streamers);
 
-    });
-
-    app.get('/getStats', async function (req, res) {
-        await main.getStats();
     });
 
     app.listen(port, () => {
