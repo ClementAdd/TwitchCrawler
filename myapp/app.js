eeb417c34396b1a0ff867ba08fa56b8b6b387dfa
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2021-05-17 14:33:22 +0200

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,7 @@
     const express = require('express');
     const path = require('path');
     const app = express();
-    const port = process.env.PORT || '8080';
+    const PORT = process.env.port||'8080';
 
     const main = require("./main");
     let streamers = await main.getStats();
